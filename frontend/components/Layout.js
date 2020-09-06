4148748faaced5be05b6539aed0fda51d4f956d6
Gobinda Thakur <gobinda.thakur@outlook.com>
2020-09-06 12:24:36 +0530

    added the pwa

--- frontend/components/Layout.js
+++ frontend/components/Layout.js
@@ -24,16 +24,34 @@ class Layout extends React.Component {
         <Head>
           <title>{title}</title>
           <meta charSet="utf-8" />
+          <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
           <meta
             name="viewport"
-            content="initial-scale=1.0, width=device-width"
+            content="width=device-width,initial-scale=1,minimum-scale=1,maximum-scale=1,user-scalable=no"
           />
+          <meta name="description" content="Description" />
+          <meta name="keywords" content="Keywords" />
           <link
             rel="stylesheet"
             href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css"
             integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm"
             crossOrigin="anonymous"
           />
+          <link rel="manifest" href="/manifest.json" />
+          <link
+            href="/favicon-16x16.png"
+            rel="icon"
+            type="image/png"
+            sizes="16x16"
+          />
+          <link
+            href="/favicon-32x32.png"
+            rel="icon"
+            type="image/png"
+            sizes="32x32"
+          />
+          <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+          <meta name="theme-color" content="#317EFB" />
           <script src="https://js.stripe.com/v3" />
         </Head>
         <header>
